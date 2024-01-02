adf051d88731b5c3b29dc5e49fb306f3a80c5499
Rafael Ferraz <rafael.ferraz@live.com>
2024-01-01 22:24:09 -0300

    Criado novos comandos. Adicionado teste para validar toast de erro ao adicionar conta com nome existente. Conteúdo até aula 58.

--- cypress/integration/Barriga/functional.spec.js
+++ cypress/integration/Barriga/functional.spec.js
@@ -1,32 +1,34 @@
 /// <reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should test at a functional level', () => {
     before(() => {
-        cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get(loc.LOGIN.USER).type('rafael@email.com')
-        cy.get(loc.LOGIN.PASSWORD).type('rafael123')
-        cy.get(loc.LOGIN.BTN_LOGIN).click()
-        cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
+        cy.login('rafael@email.com', 'rafael123')
+        cy.resetApp()
     })
 
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
     it('Should update an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
+    })
+
+    it('Should not create an account with same name', () => {
+        cy.acessarMenuConta()
+        cy.get(loc.CONTAS.NOME).type('Conta alterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
 })
